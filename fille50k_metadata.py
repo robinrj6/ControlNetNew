31e2719399621b4e7b1c15701f7e9e31edba07ec
Robin Jose Raju <robinrjr10germany@gmail.com>
2026-03-11 19:10:59 +0100

    fix: metadata.py

--- fille50k_metadata.py
+++ fille50k_metadata.py
@@ -4,10 +4,11 @@ import json
 
 with open('datasets/fill50k/metadata.jsonl', 'r') as f:
     metadata = [json.loads(line) for line in f]
-for item in metadata:
-    item['text'] = item.pop('prompt')
-    item['conditioning_file_name'] = item.pop('source')
-    item['file_name'] = item.pop('target')
+if 'prompt' in metadata[0]:  # only rename if not already done
+    for item in metadata:
+        item['text'] = item.pop('prompt')
+        item['conditioning_file_name'] = item.pop('source')
+        item['file_name'] = item.pop('target')
 with open('datasets/fill50k/metadata.jsonl', 'w') as f:
     for item in metadata:
         f.write(json.dumps(item) + '\n')
