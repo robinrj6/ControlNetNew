7801676659e8caccd2f368fdc4acc515f9f437dc
Robin Jose Raju <robinrjr10germany@gmail.com>
2026-03-11 16:57:15 +0100

    update: new metadata in train

--- train.sh
+++ train.sh
@@ -25,9 +25,9 @@ accelerate launch train_controlnet.py \
  --pretrained_model_name_or_path=$MODEL_DIR \
  --output_dir=$OUTPUT_DIR \
  --train_data_dir="$DATASET_PATH" \
- --image_column="target" \
- --caption_column="prompt" \
- --conditioning_image_column="source" \
+ --image_column="file_name" \
+ --caption_column="text" \
+ --conditioning_image_column="conditioning_file_name" \
  --resolution=512 \
  --learning_rate=1e-5 \
  --validation_image "./conditioning_image_1.png" "./conditioning_image_2.png" \
