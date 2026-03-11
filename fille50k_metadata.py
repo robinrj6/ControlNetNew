4e9dd5419b1fe76ffd722f92d3dbdeb8ab498d36
Robin Jose Raju <robinrjr10germany@gmail.com>
2026-03-11 16:55:38 +0100

    fix:metada.json

--- fille50k_metadata.py
+++ fille50k_metadata.py
@@ -2,12 +2,12 @@
 
 import json
 
-with open('fill50k/metadata.jsonl', 'r') as f:
+with open('datasets/fill50k/metadata.jsonl', 'r') as f:
     metadata = [json.loads(line) for line in f]
 for item in metadata:
     item['text'] = item.pop('prompt')
     item['conditioning_file_name'] = item.pop('source')
     item['file_name'] = item.pop('target')
-with open('fill50k/metadata.jsonl', 'w') as f:
+with open('datasets/fill50k/metadata.jsonl', 'w') as f:
     for item in metadata:
         f.write(json.dumps(item) + '\n')
