d8aff7eeb32ade0b092ecd4a51ba504c16fa50a5
Robin Jose Raju <robinrjr10germany@gmail.com>
2026-03-12 13:50:15 +0100

    update: plot training  depth

--- plot_Convergence.py
+++ plot_Convergence.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import re
 import os
 
-fileName = "controlnet_train_depth_1548565"
+fileName = "controlnet_train_depth_1548585"
 # Read the training loss values from the file
 with open(f'logs/{fileName}.err', 'r') as f:
     lines = f.readlines()
