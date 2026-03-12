1bdc0608d2d90d89bbf3c7b2631ec9b1576c1f8e
Robin Jose Raju <robinrjr10germany@gmail.com>
2026-03-12 12:18:07 +0100

    update: rename SLURM job

--- train.sh
+++ train.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
-#SBATCH --job-name=controlnet_train_fill50k
+#SBATCH --job-name=controlnet_train_depth
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --time=20:00:00
-#SBATCH --output=logs/controlnet_train_Fill50k_%j.log
-#SBATCH --error=logs/controlnet_train_Fill50k_%j.err
+#SBATCH --output=logs/controlnet_train_depth_%j.log
+#SBATCH --error=logs/controlnet_train_depth_%j.err
 
 export MODEL_DIR="models/sd15/"
 export OUTPUT_DIR="output/depth_coco_controlnet"
