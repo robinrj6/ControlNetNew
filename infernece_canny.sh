e528c97d96cf0de0a7876e43c71d754b680925d8
Robin Jose Raju <robinrjr10germany@gmail.com>
2026-03-18 21:26:32 +0100

    update: ready for all chekpoint check

--- infernece_canny.sh
+++ infernece_canny.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-#SBATCH --job-name=controlnet_fill50k_inference
+#SBATCH --job-name=controlnet_canny_inference_experiment
 #SBATCH --gres=gpu:1
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --time=20:00:00
-#SBATCH --output=logs/controlnet_fill50k_inference_%j.log
-#SBATCH --error=logs/controlnet_fill50k_inference_%j.err
+#SBATCH --output=logs/controlnet_canny_inference_experiment_%j.log
+#SBATCH --error=logs/controlnet_canny_inference_experiment_%j.err
 
 python inference_canny.py 
